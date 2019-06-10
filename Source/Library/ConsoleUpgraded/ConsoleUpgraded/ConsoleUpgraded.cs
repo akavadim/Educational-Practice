8ee6962dd56df6116b3a52310390a198fe97abff
pixel299 <vadim299@icloud.com>
2019-06-10 17:01:30 +0500

    AddProjects
    Добавлены два проекта: ConsoleUpgraded и Menu, две библиотеки написаны мной для удобства ввода данных в консоль
    (Все проекты либо ссылаются на библиотеки в папке Library, либо не используют сторонние библиотеки)

---
+++ Source/Library/ConsoleUpgraded/ConsoleUpgraded/ConsoleUpgraded.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUpgraded
+{
+    public static class UpgradedConsole
+    {
+        #region Для ввода
+
+        public static bool GetYesOrNo()
+        {
+            System.Console.WriteLine("1. Да\n2. Нет");
+            int input = GetInt(1, 2);
+            if (input == 1)
+                return true;
+            else
+                return false;
+        }
+
+        #region GetDouble
+
+        public static double GetDouble()
+        {
+            double res;
+            while (true)
+            {
+                if (double.TryParse(System.Console.ReadLine().Replace('.',','), out res))
+                    return res;
+                else System.Console.WriteLine("Введите число");
+            }
+        }
+
+        public static double GetDouble(double leftLimit)
+        {
+            while (true)
+            {
+                double res = GetDouble();
+                if (res >= leftLimit)
+                    return res;
+                else System.Console.WriteLine($"Значение не может быть меньше {leftLimit}");
+            }
+        }
+
+        public static double GetDouble(double numberStart, double numberEnd)
+        {
+            if (numberEnd < numberStart)
+                throw new ArgumentException("Число для начала отсчета не может быть больше числа конца отсчета");
+            while (true)
+            {
+                double res = GetDouble();
+                if (res >= numberStart && res <= numberEnd)
+                    return res;
+                else System.Console.WriteLine($"Значение не может быть меньше {numberStart} и больше {numberEnd}");
+            }
+        }
+
+        #endregion
+
+        #region GetInt
+
+        public static int GetInt()
+        {
+            int res;
+            while (true)
+            {
+                if (int.TryParse(System.Console.ReadLine(), out res))
+                    return res;
+                else System.Console.WriteLine("Введите целое число");
+            }
+        }
+
+        public static int GetInt(int leftLimit)
+        {
+            while (true)
+            {
+                int res = GetInt();
+                if (res >= leftLimit)
+                    return res;
+                else System.Console.WriteLine($"Значение не может быть меньше {leftLimit}");
+            }
+        }
+
+        public static int GetInt(int numberStart, int numberEnd)
+        {
+            if (numberEnd < numberStart)
+                throw new ArgumentException("Число для начала отсчета не может быть больше числа конца отсчета");
+            while (true)
+            {
+                int res = GetInt();
+                if (res >= numberStart && res <= numberEnd)
+                    return res;
+                else System.Console.WriteLine($"Значение не может быть меньше {numberStart} и больше {numberEnd}");
+            }
+        }
+
+        #endregion
+
+        public static string GetStringNotEmpty()
+        {
+            string res;
+            while (string.IsNullOrWhiteSpace(res = System.Console.ReadLine()))
+            {
+                System.Console.WriteLine("Введите что-нибудь");
+            }
+            return res;
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Library/Menu/Menu/ConsoleMenu.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    /// <summary>
+    /// Меню для коносли
+    /// </summary>
+    public class ConsoleMenu
+    {
+        /// <summary>
+        /// Выбор происходит стрелками
+        /// </summary>
+        /// <param name="items">Пункт меню</param>
+        /// <returns>Индекс выбранного элемента</returns>
+        public static int Show(string[] items)
+        {
+            return Show("", items);
+        }
+
+        /// <summary>
+        /// Выбор происходит стрелками
+        /// </summary>
+        /// <param name="items">Пункт меню</param>
+        /// <param name="headline">Заголовок меню</param>
+        /// <returns>Индекс выбранного элемента</returns>
+        public static int Show(string headline, string[] items)
+        {
+            int CurrentItem = 0;
+            if (items.Length == 0)
+                throw new ArgumentException("В меню должен быть хотя бы один пункт");
+
+            ConsoleKeyInfo keyInfo=new ConsoleKeyInfo();
+            do
+            {
+                Console.Clear();
+
+                if (headline.Length != 0)
+                    Console.WriteLine(headline+'\n');
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    if (CurrentItem == 0)
+                        CurrentItem = items.Length-1;
+                    else CurrentItem--;
+                }
+                else if(keyInfo.Key==ConsoleKey.DownArrow)
+                {
+                    if (CurrentItem == items.Length-1)
+                        CurrentItem = 0;
+                    else
+                        CurrentItem++;
+                }
+
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (i == CurrentItem)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine(items[i]);
+                        Console.ResetColor();
+                    }
+                    else
+                        Console.WriteLine(items[i]);
+                }
+            } while ((keyInfo = Console.ReadKey()).Key != (ConsoleKey.Enter));
+            return CurrentItem;
+        }
+    }
+}
